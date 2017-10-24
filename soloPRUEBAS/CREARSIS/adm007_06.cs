d8238037ddfd0ce015a31ea80af2b65fdaca6ec5
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2017-10-24 14:14:47 -0400

    FRM CREADOS

---
+++ soloPRUEBAS/CREARSIS/adm007_06.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS
+{
+    public partial class adm007_06 : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        public adm007_06()
+        {
+            InitializeComponent();
+        }
+    }
+}
