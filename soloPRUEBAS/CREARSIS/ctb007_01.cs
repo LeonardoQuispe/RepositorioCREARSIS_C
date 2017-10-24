75104fd3c0c6f9caf61ff0eeba8bfbd05102b8f4
Leonardo Quispe Morales <leonardito161718@hotmail.com>
2017-10-24 17:18:57 -0400

    creando FORMS dosificaciones

---
+++ soloPRUEBAS/CREARSIS/ctb007_01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS
+{
+    public partial class ctb007_01 : Form
+    {
+        public ctb007_01()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ soloPRUEBAS/CREARSIS/ctb007_03a.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS
+{
+    public partial class ctb007_03a : Form
+    {
+        public ctb007_03a()
+        {
+            InitializeComponent();
+        }
+    }
+}
