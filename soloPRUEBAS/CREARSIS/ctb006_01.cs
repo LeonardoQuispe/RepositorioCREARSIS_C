1410e57d0833a452f931f78b3acab58bb6b8f1dd
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2017-10-25 08:33:29 -0400

    ctb006 creado

---
+++ soloPRUEBAS/CREARSIS/ctb006_01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//REFERENCIAS
+
+
+namespace CREARSIS
+{
+    public partial class ctb006_01 : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        public ctb006_01()
+        {
+            InitializeComponent();
+        }
+    }
+}
