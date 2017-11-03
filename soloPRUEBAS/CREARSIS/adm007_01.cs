2f66ecdbd9f98ef5fb789e0d19f290bda9c95967
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2017-11-03 10:53:10 -0400

    switch(row CORREGIDO .TO STRING( )

--- soloPRUEBAS/CREARSIS/adm007_01.cs
+++ soloPRUEBAS/CREARSIS/adm007_01.cs
@@ -97,7 +97,7 @@ namespace CREARSIS
 
             foreach (DataRow row in tab_adm007.Rows)
             {
-                switch (row["va_est_ado"])
+                switch (row["va_est_ado"].ToString())
                 {
                     case "H":
                         va_est_ado = "Habilitada";
