d6865815419088b862795e6333d2442e55198c3b
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2017-11-10 08:46:55 -0400

    formularios nuevos creados

---
+++ soloPRUEBAS/CREARSIS/adm013_02a.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS
+{
+    public partial class adm013_02a : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        public adm013_02a()
+        {
+            InitializeComponent();
+        }
+    }
+}
