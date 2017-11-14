bc909f00653b3e97077f27c98fbd39793cd514c5
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2017-11-14 10:07:01 -0400

    nuevo

---
+++ soloPRUEBAS/CREARSIS/adm014_09.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CREARSIS
+{
+    partial class adm014_09
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.bt_imp_xls = new DevComponents.DotNetBar.ButtonX();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dg_res_ult = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            this.DIA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Enero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gb_ctr_frm = new System.Windows.Forms.GroupBox();
+            this.bt_can_cel = new DevComponents.DotNetBar.ButtonX();
+            this.bt_ace_pta = new DevComponents.DotNetBar.ButtonX();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dg_res_ult)).BeginInit();
+            this.gb_ctr_frm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bt_imp_xls
+            // 
+            this.bt_imp_xls.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.bt_imp_xls.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.bt_imp_xls.Location = new System.Drawing.Point(102, 43);
+            this.bt_imp_xls.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt_imp_xls.Name = "bt_imp_xls";
+            this.bt_imp_xls.Size = new System.Drawing.Size(75, 27);
+            this.bt_imp_xls.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.bt_imp_xls.TabIndex = 37;
+            this.bt_imp_xls.Text = "Importar";
+            this.bt_imp_xls.Click += new System.EventHandler(this.bt_imp_xls_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dg_res_ult);
+            this.groupBox1.Controls.Add(this.bt_imp_xls);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.groupBox1.Location = new System.Drawing.Point(2, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 310);
+            this.groupBox1.TabIndex = 44;
+            this.groupBox1.TabStop = false;
+            // 
+            // dg_res_ult
+            // 
+            this.dg_res_ult.AllowUserToAddRows = false;
+            this.dg_res_ult.AllowUserToDeleteRows = false;
+            this.dg_res_ult.AllowUserToOrderColumns = true;
+            this.dg_res_ult.AllowUserToResizeRows = false;
+            this.dg_res_ult.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(242)))));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dg_res_ult.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dg_res_ult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DIA,
+            this.Enero});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(242)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dg_res_ult.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dg_res_ult.EnableHeadersVisualStyles = false;
+            this.dg_res_ult.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(157)))));
+            this.dg_res_ult.Location = new System.Drawing.Point(41, 102);
+            this.dg_res_ult.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dg_res_ult.MultiSelect = false;
+            this.dg_res_ult.Name = "dg_res_ult";
+            this.dg_res_ult.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dg_res_ult.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dg_res_ult.RowHeadersVisible = false;
+            this.dg_res_ult.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dg_res_ult.Size = new System.Drawing.Size(208, 179);
+            this.dg_res_ult.TabIndex = 44;
+            this.dg_res_ult.TabStop = false;
+            // 
+            // DIA
+            // 
+            this.DIA.HeaderText = "Fecha";
+            this.DIA.Name = "DIA";
+            this.DIA.ReadOnly = true;
+            this.DIA.Width = 70;
+            // 
+            // Enero
+            // 
+            this.Enero.HeaderText = "T.C - Bs/Ufv";
+            this.Enero.Name = "Enero";
+            this.Enero.ReadOnly = true;
+            // 
+            // gb_ctr_frm
+            // 
+            this.gb_ctr_frm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(242)))));
+            this.gb_ctr_frm.Controls.Add(this.bt_can_cel);
+            this.gb_ctr_frm.Controls.Add(this.bt_ace_pta);
+            this.gb_ctr_frm.ForeColor = System.Drawing.Color.Black;
+            this.gb_ctr_frm.Location = new System.Drawing.Point(12, 318);
+            this.gb_ctr_frm.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gb_ctr_frm.Name = "gb_ctr_frm";
+            this.gb_ctr_frm.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gb_ctr_frm.Size = new System.Drawing.Size(301, 57);
+            this.gb_ctr_frm.TabIndex = 79;
+            this.gb_ctr_frm.TabStop = false;
+            // 
+            // bt_can_cel
+            // 
+            this.bt_can_cel.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.bt_can_cel.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.bt_can_cel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bt_can_cel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_can_cel.Location = new System.Drawing.Point(153, 22);
+            this.bt_can_cel.Name = "bt_can_cel";
+            this.bt_can_cel.Size = new System.Drawing.Size(94, 23);
+            this.bt_can_cel.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.bt_can_cel.Symbol = "";
+            this.bt_can_cel.SymbolColor = System.Drawing.Color.Maroon;
+            this.bt_can_cel.SymbolSize = 15F;
+            this.bt_can_cel.TabIndex = 102;
+            this.bt_can_cel.Text = "Cancelar";
+            this.bt_can_cel.Click += new System.EventHandler(this.bt_can_cel_Click);
+            // 
+            // bt_ace_pta
+            // 
+            this.bt_ace_pta.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.bt_ace_pta.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.bt_ace_pta.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_ace_pta.Location = new System.Drawing.Point(48, 22);
+            this.bt_ace_pta.Name = "bt_ace_pta";
+            this.bt_ace_pta.Size = new System.Drawing.Size(99, 23);
+            this.bt_ace_pta.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.bt_ace_pta.Symbol = "";
+            this.bt_ace_pta.SymbolColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.bt_ace_pta.SymbolSize = 15F;
+            this.bt_ace_pta.TabIndex = 101;
+            this.bt_ace_pta.Text = "Aceptar";
+            // 
+            // adm014_09
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 397);
+            this.Controls.Add(this.gb_ctr_frm);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.Name = "adm014_09";
+            this.Text = "Importar Excel T.C - Bs/Ufv";
+            this.TitleText = "Importar Excel T.C - Bs/Ufv";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dg_res_ult)).EndInit();
+            this.gb_ctr_frm.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.ButtonX bt_imp_xls;
+        private System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.GroupBox gb_ctr_frm;
+        internal DevComponents.DotNetBar.ButtonX bt_can_cel;
+        internal DevComponents.DotNetBar.ButtonX bt_ace_pta;
+        internal DevComponents.DotNetBar.Controls.DataGridViewX dg_res_ult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DIA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Enero;
+    }
+}
---
+++ soloPRUEBAS/CREARSIS/adm014_09.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//REFERENCIAS
+using DATOS.ADM;
+using DevComponents.DotNetBar;
+using CREARSIS.GLOBAL;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CREARSIS
+{
+    public partial class adm014_09 : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        #region VARIABLES
+
+        public dynamic vg_frm_pad;
+
+        #endregion
+
+        #region INSTANCIAS
+
+        mg_glo_bal o_mg_glo_bal = new mg_glo_bal();
+
+        #endregion
+
+
+        #region METODOS
+
+        void fu_imp_xls()
+        {
+
+            try
+            {
+                string ruta = "";
+
+
+                OpenFileDialog openfile1 = new OpenFileDialog();
+                openfile1.Filter = "Libro de Excel 97-2003|*.xls|Libro de Excel|*.xlsx";
+                openfile1.Title = "Seleccione el Libro de Excel";
+                if (openfile1.ShowDialog() == DialogResult.OK)
+                {
+                    ruta = openfile1.FileName;
+
+
+                    //creando una instancia para el objeto de excel 
+                    Excel.Application obj_xls = new Excel.Application();
+
+                    //pasando el objeto a un libro de excel
+                    Excel.Workbook libro_xls = obj_xls.Workbooks.Open(ruta);
+
+                    //Elijiendo la hoja del libro de excel elegido
+                    Excel.Worksheet hoja_xls = (Excel.Worksheet)libro_xls.Worksheets[1];
+
+                    //asignando el rango de filas y columnas usadas en la hoja de excel
+                    Excel.Range xlsRange = hoja_xls.UsedRange;
+
+                    ////recuperando el nombre del libro, de la hoja, y el año seleccionado
+                    //lbl_libro_xls.Text = libro_xls.Name;
+                    //lbl_hoja_xls.Text = hoja_xls.Name;
+                    //string tmp = xlsRange[2, 1].Value.ToString();
+                    //lbl_año.Text = tmp.Substring(4, 4);
+
+
+
+                    dg_res_ult.Rows.Clear();
+
+                    //cargando el contenido 
+                    int filas = 30;
+                    int columnas = 12;
+
+                    for (int i = 0; i <= filas; i++)
+                    {
+                        dg_res_ult.Rows.Add();
+
+                        for (int j = 0; j <= columnas; j++)
+                        {
+                            dg_res_ult[j, i].Value = xlsRange[i + 7, j + 1].Value ?? "";
+                        }
+                    }
+
+
+                    libro_xls.Close();
+                    obj_xls.Quit();
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBoxEx.Show(ex.Message);
+            }
+
+        }
+
+        #endregion
+
+        public adm014_09()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_imp_xls_Click(object sender, EventArgs e)
+        {
+            fu_imp_xls();
+        }
+
+        private void bt_can_cel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
