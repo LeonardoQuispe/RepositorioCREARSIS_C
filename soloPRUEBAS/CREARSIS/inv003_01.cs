a74c39551bd4985c46ebfa314c3988f89db9d0ff
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2017-11-20 10:14:59 -0400

    all forms

---
+++ soloPRUEBAS/CREARSIS/inv003_01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS
+{
+    public partial class inv003_01 : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        public inv003_01()
+        {
+            InitializeComponent();
+        }
+    }
+}
