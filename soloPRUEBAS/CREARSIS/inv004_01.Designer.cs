84f6cf26c430b42b5dbbbe0cb9ea9959dc6f8771
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2017-11-20 09:34:30 -0400

    Nuevos Forms

---
+++ soloPRUEBAS/CREARSIS/inv004_01.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace CREARSIS
+{
+    partial class inv004_01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // inv004_01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 383);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "inv004_01";
+            this.Text = "Buscar marca";
+            this.TitleText = "Buscar marca";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ soloPRUEBAS/CREARSIS/inv004_03.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS
+{
+    public partial class inv004_03 : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        public inv004_03()
+        {
+            InitializeComponent();
+        }
+    }
+}
