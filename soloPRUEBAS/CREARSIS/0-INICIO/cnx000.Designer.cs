f6a9ae77dc9383297e38a612608deaf1f06c539b
Leonardo Quispe Morales <leonardito161718@hotmail.com>
2018-01-03 13:39:48 -0400

    l

--- soloPRUEBAS/CREARSIS/0-INICIO/cnx000.Designer.cs
+++ soloPRUEBAS/CREARSIS/0-INICIO/cnx000.Designer.cs
@@ -46,6 +46,7 @@
             this.styleManager1 = new DevComponents.DotNetBar.StyleManager(this.components);
             this.lk_fan_pag = new System.Windows.Forms.LinkLabel();
             this.line1 = new DevComponents.DotNetBar.Controls.Line();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -282,6 +283,13 @@
             this.line1.Text = "line1";
             this.line1.VerticalLine = true;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(126, 73);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 90;
+            // 
             // cnx000
             // 
             this.AcceptButton = this.bt_ace_pta;
@@ -290,6 +298,7 @@
             this.CancelButton = this.bt_can_cel;
             this.ClientSize = new System.Drawing.Size(510, 211);
             this.ControlBox = false;
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.lk_fan_pag);
             this.Controls.Add(this.bt_ver_pss);
             this.Controls.Add(this.bt_can_cel);
@@ -306,6 +315,7 @@
             this.Controls.Add(this.line1);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -338,5 +348,6 @@
         private DevComponents.DotNetBar.StyleManager styleManager1;
         private System.Windows.Forms.LinkLabel lk_fan_pag;
         private DevComponents.DotNetBar.Controls.Line line1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
