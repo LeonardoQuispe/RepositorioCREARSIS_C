3414d38eebb7394520acccc76b8b7e88f87fbd1e
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2018-01-06 13:04:13 -0400

    .

--- soloPRUEBAS/CREARSIS/4-INV/inv002(pro)/inv002_02.cs
+++ soloPRUEBAS/CREARSIS/4-INV/inv002(pro)/inv002_02.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+
 //REFERENCIAS
 using DATOS;
+using System.Transactions;
 using DevComponents.DotNetBar;
 
 namespace CREARSIS._4_INV.inv002_pro_
@@ -19,54 +21,38 @@ namespace CREARSIS._4_INV.inv002_pro_
         #region VARIABLES
 
         public dynamic vg_frm_pad;
+        DataTable tab_adm002;
+        DataTable tab_adm003;
+        DataTable tab_adm004;
+        DataTable tab_ctb007;
+
         string err_msg = "";
-        DataTable tab_adm007;
-        DataTable tab_inv010;
 
         #endregion
 
         #region INSTANCIAS
 
-        c_inv010 o_inv010 = new c_inv010();
-        c_adm007 o_adm007 = new c_adm007();
+        c_adm002 o_adm002 = new c_adm002();
+        c_adm003 o_adm003 = new c_adm003();
+        c_adm004 o_adm004 = new c_adm004();
+        c_adm005 o_adm005 = new c_adm005();
+        c_ctb007 o_ctb007 = new c_ctb007();
         _01_mg_glo_bal o_mg_glo_bal = new _01_mg_glo_bal();
 
         #endregion
 
-        #region METODOS
-        public void fu_rec_suc(string cod_suc)
+        #region MÉTODOS
+
+        public void fu_ini_frm()
         {
-            string tmp = "";
+            cb_tip_num.SelectedIndex = 0;
+            tb_for_mat.Text = "0";
+            cb_nro_cop.SelectedIndex = 0;
+            //tb_nro_dos.Text = "0";
+            cb_for_log.SelectedIndex = 0;
 
-            if (cod_suc.Trim() == "")
-            {
-                tb_cod_sucu.Text = "";
-                tb_nom_sucu.Text = "** NO existe";
-                tb_cod_gru.Text = "00" + tb_cod_gru.Text[2].ToString() + tb_cod_gru.Text[3].ToString();
-                return;
-            }
+            tb_cod_doc.Focus();
 
-            tab_adm007 = o_adm007._05(cod_suc);
-            if (tab_adm007.Rows.Count == 0)
-            {
-                tb_cod_sucu.Text = "";
-                tb_nom_sucu.Text = "** NO existe";
-                tb_cod_gru.Text = "00" + tb_cod_gru.Text[2].ToString() + tb_cod_gru.Text[3].ToString();
-                return;
-            }
-
-            tb_cod_sucu.Text = cod_suc;
-            tb_nom_sucu.Text = tab_adm007.Rows[0]["va_nom_suc"].ToString();
-
-            tmp = tb_cod_sucu.Text.PadLeft(2, '0');
-
-            tb_cod_gru.Text = tmp[0].ToString() + tmp[1].ToString() + tb_cod_gru.Text[2].ToString() + tb_cod_gru.Text[3].ToString();
-
-
-        }
-        void fu_ini_frm()
-        {
-            tb_cod_sucu.Focus();
         }
 
         /// <summary>
@@ -74,14 +60,20 @@ namespace CREARSIS._4_INV.inv002_pro_
         /// </summary>
         public void fu_lim_frm()
         {
-            tb_cod_sucu.Clear();
-            tb_nro_gru.Clear();
-            tb_cod_gru.Clear();
-            tb_nom_gru.Clear();
-            tb_des_gru.Clear();
+            tb_cod_doc.Clear();
+            tb_nom_doc.Clear();
+            tb_nro_tal.Clear();
+            tb_nom_tal.Clear();
+            tb_cod_ges.Clear();
+            tb_for_mat.Text = "0";
+            tb_nro_aut.Text = "0";
+            tb_fir_ma1.Clear();
+            tb_fir_ma2.Clear();
+            tb_fir_ma3.Clear();
+            tb_fir_ma4.Clear();
 
+            tb_nro_tal.Focus();
 
-            tb_cod_sucu.Focus();
         }
 
         /// <summary>
@@ -89,69 +81,153 @@ namespace CREARSIS._4_INV.inv002_pro_
         /// </summary>
         public string fu_ver_dat()
         {
-
-            //**Verifica Codigo de la Sucursal-----------------------------------
             int tmp;
+            Int64 tmp2;
 
-            if (tb_cod_sucu.Text.Trim() == "")
+            //** Verifica Documento---------------------------------
+            if (tb_cod_doc.Text.Trim() == "")
             {
-                tb_cod_sucu.Focus();
-                return "Debes proporcionar el código de la Sucursal";
+                tb_cod_doc.Focus();
+                return "Debes proporcionar el codigo de documento";
             }
 
-            if (int.TryParse(tb_cod_sucu.Text.Trim(), out tmp) == false)
+            tab_adm003 = o_adm003._05(tb_cod_doc.Text);
+            if (tab_adm003.Rows.Count == 0)
             {
-                tb_cod_sucu.Focus();
-                return "El Codigo de la Sucursal NO es valido";
+                tb_cod_doc.Focus();
+                return "El documento NO se encuentra registrado";
+            }
+            if (tab_adm003.Rows[0]["va_est_ado"].ToString() == "N")
+            {
+                tb_cod_doc.Focus();
+                return "El documento se encuentra Deshabilitado";
+            }
+            //**-----------------------------------------------------
+
+            //**Verifica Talonario-----------------------------------
+            if (int.TryParse(tb_nro_tal.Text.Trim(), out tmp) == false)
+            {
+                tb_nro_tal.Focus();
+                return "El Nro de Talonario debe ser numérico";
             }
 
-            tab_adm007 = o_adm007._05(tb_cod_sucu.Text);
-            if (tab_adm007.Rows.Count == 0)
+            if (tb_nro_tal.Text == "0")
             {
-                tb_cod_sucu.Focus();
-                return "El Codigo de la Sucursal NO se encuentra registrado";
+                tb_nro_tal.Focus();
+                return "El Nro de Talonario debe ser diferente de 0";
             }
 
-            if (tab_adm007.Rows[0]["va_est_ado"].ToString() == "N")
+            tab_adm004 = o_adm004._05(tb_cod_doc.Text, int.Parse(tb_nro_tal.Text));
+            if (tab_adm004.Rows.Count != 0)
             {
-                tb_cod_sucu.Focus();
-                return "El Codigo de la Sucursal se encuentra Deshabilitado";
+                tb_nro_tal.Focus();
+                return "El Talonario ya se encuentra registrado";
+            }
+            //**-----------------------------------------------------
+
+            //**Verifica que la gestion sea valida-------------------
+            if (int.TryParse(tb_cod_ges.Text.Trim(), out tmp) == false)
+            {
+                tb_cod_ges.Focus();
+                return "La Gestion no es valida";
             }
 
-            //VERIFICA numero de Grupo
-
-            if (string.IsNullOrWhiteSpace(tb_nro_gru.Text))
+            tab_adm002 = o_adm002._05(int.Parse(tb_cod_ges.Text));
+            if (tab_adm002.Rows.Count == 0)
             {
-                tb_nro_gru.Focus();
-                return "Debes proporcionar el Número del Grupo de Almacén";
+                tb_cod_ges.Focus();
+                return "La Gestión NO se encuentra registrada";
+            }
+            //**-----------------------------------------------------
+
+            //**Verifica formato-------------------------------------
+            if (int.TryParse(tb_for_mat.Text.Trim(), out tmp) == false)
+            {
+                tb_for_mat.Focus();
+                return "El formato debe ser numerico";
+            }
+            //**-----------------------------------------------------
+
+            //**Verifica Nro de Dosificacion-------------------------
+            if (tb_nro_aut.Text.Trim() == "")
+            {
+                tb_cod_doc.Focus();
+                return "Debes proporcionar el codigo de Dosificación";
             }
 
-            if (int.Parse(tb_nro_gru.Text) <= 0)
+            if (tb_nro_aut.Text != "0")
             {
-                tb_nro_gru.Focus();
-                return "El Número del Grupo de Almacén debe ser mayor a cero";
+                tab_ctb007 = o_ctb007._05(Int64.Parse(tb_nro_aut.Text));
+                if (tab_ctb007.Rows.Count == 0)
+                {
+                    tb_nro_aut.Focus();
+                    return "La Dosificación NO se encuentra registrada";
+                }
+                if (tab_ctb007.Rows[0]["va_est_ado"].ToString() == "N")
+                {
+                    tb_nro_aut.Focus();
+                    return "La Dosificación se encuentra Deshabilitada";
+                }
             }
 
-            //**Verifica Codigo de GRUPO
-            tab_inv010 = o_inv010._05(int.Parse(tb_cod_gru.Text));
-            if (tab_inv010.Rows.Count != 0)
-            {
-                tb_cod_gru.Focus();
-                return "El codigo del Grupo de Almacén ya se encuentra registrado";
-            }
+            //if (Int64.TryParse(tb_nro_aut.Text.Trim(), out tmp2) == false)
+            //{
+            //    tb_nro_aut.Focus();
+            //    return "El Nro de Dosificación debe ser numérico";
+            //}
 
-            //**Verifica el nombre del grupo de Almacén
-            if (tb_nom_gru.Text.Trim() == "")
-            {
-                tb_nom_gru.Focus();
-                return "Debes proporcionar el nombre  del Grupo de Almacén";
-            }
-
-
-
+            //if (tb_nro_aut.Text.Trim() == "0")
+            //{
+            //    tb_nro_aut.Focus();
+            //    return "El Nro de Dosificación debe ser diferente de 0";
+            //}           
 
             return null;
         }
+
+
+        public void fu_rec_doc(string cod_doc)
+        {
+            if (cod_doc.Trim() == "")
+            {
+                tb_nom_doc.Text = "** NO existe";
+                return;
+            }
+
+            tab_adm003 = o_adm003._05(cod_doc);
+            if (tab_adm003.Rows.Count == 0)
+            {
+                tb_nom_doc.Text = "** NO existe";
+                return;
+            }
+
+            tb_cod_doc.Text = tab_adm003.Rows[0]["va_cod_doc"].ToString();
+            tb_nom_doc.Text = tab_adm003.Rows[0]["va_nom_doc"].ToString();
+
+        }
+
+        public void fu_rec_dos(string cod_dos)
+        {
+            if (cod_dos.Trim() == "")
+            {
+                tb_nro_aut.Text = "";
+                return;
+            }
+
+            if (cod_dos != "0")
+            {
+                tab_ctb007 = o_ctb007._05(Int64.Parse(cod_dos));
+                if (tab_ctb007.Rows.Count == 0)
+                {
+                    tb_nro_aut.Text = "";
+                    return;
+                }
+
+                tb_nro_aut.Text = cod_dos;
+            }
+        }
+
+
         #endregion
 
         #region EVENTOS
