a04f909dbfde34a1b912da750a6bcadb707e07a0
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2018-01-18 20:44:34 -0400

    agregando form

---
+++ soloPRUEBAS/CREARSIS/4-INV/inv011(alm)/inv011_03a.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS._4_INV.inv011_alm_
+{
+    public partial class inv011_03a : Form
+    {
+        public inv011_03a()
+        {
+            InitializeComponent();
+        }
+    }
+}
