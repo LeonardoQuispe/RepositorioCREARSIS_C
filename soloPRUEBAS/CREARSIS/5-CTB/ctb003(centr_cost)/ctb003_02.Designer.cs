77381f4a043a6e202f0a07a0698c0dbd81be0116
Leonardo Quispe Morales <leonardito161718@hotmail.com>
2018-01-29 11:20:30 -0400

    creando forms ABM ctb003 (Centro de Costos)

---
+++ soloPRUEBAS/CREARSIS/5-CTB/ctb003(centr_cost)/ctb003_02.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace CREARSIS._5_CTB.ctb003_centr_cost_
+{
+    partial class ctb003_02
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ctb003_02
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.DoubleBuffered = true;
+            this.Name = "ctb003_02";
+            this.Text = "ctb003_02";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ soloPRUEBAS/CREARSIS/5-CTB/ctb003(centr_cost)/ctb003_04.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS._5_CTB.ctb003_centr_cost_
+{
+    public partial class ctb003_04 : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        public ctb003_04()
+        {
+            InitializeComponent();
+        }
+    }
+}
