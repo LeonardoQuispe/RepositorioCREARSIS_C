519dac97b843542eda3882fd42199987837fcecb
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2018-01-29 14:52:48 -0400

    cREANDO FORMULARIOS Y CLASES CTB002

---
+++ soloPRUEBAS/CREARSIS/5-CTB/ctb002(cap_agru)/ctb002_04.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS._5_CTB.ctb002_cap_agru_
+{
+    public partial class ctb002_04 : Form
+    {
+        public ctb002_04()
+        {
+            InitializeComponent();
+        }
+    }
+}
