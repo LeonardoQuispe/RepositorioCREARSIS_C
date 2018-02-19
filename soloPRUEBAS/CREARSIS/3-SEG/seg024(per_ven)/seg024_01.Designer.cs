ce1c532d24374d2536aa2a09f9cba8632af4cf81
Leonardo Quispe Morales <leonardito161718@outlook.com>
2018-02-19 19:01:49 -0400

    creando seg024

---
+++ soloPRUEBAS/CREARSIS/3-SEG/seg024(per_ven)/seg024_01.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace CREARSIS._3_SEG.seg024_per_ven_
+{
+    partial class seg024_01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // seg024_01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 410);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.Name = "seg024_01";
+            this.Text = "seg024_01";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ soloPRUEBAS/CREARSIS/3-SEG/seg024(per_ven)/seg024_01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS._3_SEG.seg024_per_ven_
+{
+    public partial class seg024_01 : Form
+    {
+        public seg024_01()
+        {
+            InitializeComponent();
+        }
+    }
+}
