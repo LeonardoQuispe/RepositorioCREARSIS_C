c5503c3f8661f97984fcf37eab4e6dd8b4e9228c
Aldair Guzman Bolaños <aldairguzman1225@gmail.com>
2018-02-20 14:09:50 -0400

    Agregando forms

---
+++ soloPRUEBAS/CREARSIS/3-SEG/seg002(mod_sis)/seg002_06.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS._3_SEG.seg002_mod_sis_
+{
+    public partial class seg002_06 : Form
+    {
+        public seg002_06()
+        {
+            InitializeComponent();
+        }
+    }
+}
