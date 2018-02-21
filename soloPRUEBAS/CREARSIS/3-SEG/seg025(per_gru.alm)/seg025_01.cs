466bbb349e0d7a37abcd42c5148e4fe316ed59ab
Leonardo Quispe Morales <leonardito161718@outlook.com>
2018-02-21 16:08:52 -0400

    creando seg025 y seg026

---
+++ soloPRUEBAS/CREARSIS/3-SEG/seg025(per_gru.alm)/seg025_01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS._3_SEG.seg025_per_gru.alm_
+{
+    public partial class seg025_01 : Form
+    {
+        public seg025_01()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ soloPRUEBAS/CREARSIS/3-SEG/seg026(per_alm)/seg026_01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CREARSIS._3_SEG.seg026_per_alm_
+{
+    public partial class seg026_01 : Form
+    {
+        public seg026_01()
+        {
+            InitializeComponent();
+        }
+    }
+}
